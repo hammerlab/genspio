b2a6b2a54e5b04c376364eb1bd96837c97df578d
Sebastien Mondet <seb@mondet.org>
2016-09-23 18:22:03 -0400

    Develop the GADT-based `exp2.ml` experiement

--- exp2.ml
+++ exp2.ml
@@ -7,51 +7,6 @@ module String = Sosa.Native_string
 
 *)
 
-module Script = struct
-
-  type _ t =
-    | Exec: string list -> unit t
-    | Bool_operator: bool t * [ `And | `Or ] * bool t -> bool t
-    | Succeed: 'a t -> bool t
-    | Noop: 'a t
-    | If: bool t * 'a t * 'a t -> 'a t
-    | Seq: unit t list -> unit t
-  module Construct = struct
-    let exec l = Exec l
-    let (&&&) a b = Bool_operator (a, `And, b)
-    let (|||) a b = Bool_operator (a, `Or, b)
-    let (~$) x = Succeed x
-    let nop = Noop
-    let if_then_else a b c = If (a, b, c)
-    let if_then a b = if_then_else a b nop
-    let seq l = Seq l
-  end
-
-  let ex1 =
-    Exec ["ls"]
-  let ex2 = Construct.(
-      ~$ ex1 &&& ~$ (seq [exec ["ls"]; exec ["bash"; "-c"; "exit 2"]]) 
-    )
-  let rec to_one_liner: type a. a t -> string =
-    fun e ->
-      match e with
-      | Exec l -> List.map l ~f:Filename.quote |> String.concat ~sep:" "
-      | Succeed c ->
-        sprintf "%s ; if [ $? -ne 0 ] ; then echo Fail; exit 2 ; fi"
-          (to_one_liner c)
-      | Bool_operator (a, op, b) ->
-        sprintf "( %s %s %s )"
-          (to_one_liner a)
-          (match op with `And -> "&&" | `Or -> "||")
-          (to_one_liner b)
-      | Noop -> "printf ''"
-      | If (c, t, e) ->
-        sprintf "if %s ; then %s ; else %s ; fi"
-          (to_one_liner c) (to_one_liner t) (to_one_liner e)
-      | Seq l ->
-        String.concat (List.map l ~f:to_one_liner) ~sep:" ; "
-end
-
 
 module Test = struct
   open Pvem_lwt_unix.Deferred_result
@@ -97,6 +52,125 @@ module Test = struct
     return ()
 end
 
+module Script = struct
+
+  type _ t =
+    | Exec: string list -> unit t
+    | Bool_operator: bool t * [ `And | `Or ] * bool t -> bool t
+    | Not: bool t -> bool t
+    | Succeed: { expr: 'a t; exit_with: int} -> bool t
+    | Noop: 'a t
+    | If: bool t * 'a t * 'a t -> 'a t
+    | Seq: unit t list -> unit t
+    | Write_output: { expr: unit t; path: string} -> unit t
+
+  module Construct = struct
+    let exec l = Exec l
+    let (&&&) a b = Bool_operator (a, `And, b)
+    let (|||) a b = Bool_operator (a, `Or, b)
+    let succeed ?(exit_with = 2) expr = Succeed {expr; exit_with}
+    let (~$) x = succeed x
+    let nop = Noop
+    let if_then_else a b c = If (a, b, c)
+    let if_then a b = if_then_else a b nop
+    let seq l = Seq l
+
+    let not t = Not t
+
+    let echo fmt =
+      ksprintf (fun s -> exec ["echo"; s]) fmt
+
+    let file_exists p =
+      exec ["test"; "-f"; p] |> succeed
+
+    let switch: type a. (bool t * a t) list -> default: a t -> a t =
+      fun conds ~default ->
+        List.fold_right conds ~init:default ~f:(fun (x, body) prev ->
+            if_then_else x body prev)
+
+    let write_stdout ~path expr = Write_output {expr; path}
+  end
+
+  let rec to_one_liner: type a. a t -> string =
+    fun e ->
+      match e with
+      | Exec l -> List.map l ~f:Filename.quote |> String.concat ~sep:" "
+      | Succeed {expr; exit_with} ->
+        sprintf "%s ; ( if [ $? -ne 0 ] ; then exit %d ; else exit 0 ; fi )"
+          (to_one_liner expr) exit_with
+      | Bool_operator (a, op, b) ->
+        sprintf "{ %s %s %s }"
+          (to_one_liner a)
+          (match op with `And -> "&&" | `Or -> "||")
+          (to_one_liner b)
+      | Noop -> "printf ''"
+      | If (c, t, e) ->
+        sprintf "if { %s } ; then %s ; else %s ; fi"
+          (to_one_liner c) (to_one_liner t) (to_one_liner e)
+      | Seq l ->
+        String.concat (List.map l ~f:to_one_liner) ~sep:" ; "
+      | Not t ->
+        sprintf "! { %s }" (to_one_liner t)
+      | Write_output { expr; path } ->
+        sprintf " ( %s ) > %s" (to_one_liner expr) path
+
+  let exits n c =
+    Test.command (to_one_liner c) [`Exits_with n]
+  let tests =
+    let exit n = Construct.exec ["exit"; Int.to_string n] in
+    let return n =
+      Construct.exec ["bash"; "-c"; sprintf "exit %d" n] in
+    [
+      exits 0 (Exec ["ls"]);
+      exits 18 Construct.(
+          ~$ (exec ["ls"])
+          &&& succeed ~exit_with:18 (seq [
+              exec ["ls"];
+              exec ["bash"; "-c"; "exit 2"]])
+        );
+      exits 23 Construct.(
+          seq [
+            if_then_else (file_exists "/etc/passwd")
+              (exit 23)
+              (exit 1);
+            exit 2;
+          ]
+        );
+      exits 23 Construct.(
+          seq [
+            if_then_else (file_exists "/etc/passwd" |> not)
+              (exit 1)
+              (exit 23);
+            exit 2;
+          ]
+        );
+      exits 20 Construct.(
+          switch ~default:(return 18) [
+            file_exists "/djlsjdseij", return 19;
+            file_exists "/etc/passwd", return 20;
+            file_exists "/djlsjdseij", return 21;
+          ]
+        );
+      exits 0 Construct.(
+          let path = "/tmp/bouh" in
+          seq [
+            if_then (file_exists path)
+              begin
+                exec ["rm"; "-f"; path]
+              end;
+            write_stdout ~path (seq [
+                echo "bouh";
+                exec ["ls"; "-la"];
+              ]);
+            if_then (file_exists path |> not)
+              begin
+                exit 1
+              end;
+          ]);
+    ]
+end
+
+
 let posix_sh_tests = [
   Test.command "ls" [`Exits_with 0];
 ]
@@ -106,6 +180,7 @@ let posix_sh_tests = [
 let () =
   let tests =
     posix_sh_tests
+    @ Script.tests
   in
   begin match Lwt_main.run (Test.run tests) with
   | `Ok () -> printf "Done.\n%!"
