c3002572a2923da9cc70f18ac68b7b9e2914aeb3
Sebastien Mondet <seb@mondet.org>
2016-10-05 16:08:27 -0400

    Add Travis build script and configuration

---
+++ tools/travis_ci_test.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+
+
+travis_install_on_linux () {
+    # Install OCaml and OPAM PPAs
+    export ppa=avsm/ocaml42+opam12
+
+    echo "yes" | sudo add-apt-repository ppa:$ppa
+    sudo apt-get update -qq
+
+    export opam_init_options="--comp=$OCAML_VERSION"
+    sudo apt-get install -qq  opam time git
+}
+
+travis_install_on_osx () {
+    curl -OL "http://xquartz.macosforge.org/downloads/SL/XQuartz-2.7.6.dmg"
+    sudo hdiutil attach XQuartz-2.7.6.dmg
+    sudo installer -verbose -pkg /Volumes/XQuartz-2.7.6/XQuartz.pkg -target /
+
+    brew update
+    brew install opam
+    export opam_init_options="--comp=$OCAML_VERSION"
+}
+
+
+case $TRAVIS_OS_NAME in
+  osx) travis_install_on_osx ;;
+  linux) travis_install_on_linux ;;
+  *) echo "Unknown $TRAVIS_OS_NAME"; exit 1
+esac
+
+# configure and view settings
+export OPAMYES=1
+echo "ocaml -version"
+ocaml -version
+echo "opam --version"
+opam --version
+echo "git --version"
+git --version
+
+# install OCaml packages
+opam init $opam_init_options
+eval `opam config env`
+
+opam update
+
+opam pin add genspio .
+opam install genspio
+
+opam install ppx_deriving
+opam install pvem_lwt_unix
+
+export OCAMLPARAM='warn-error=Ad-58,_'
+export WITH_TESTS=true
+make byte
+make native
+
+./genspio-test.byte
