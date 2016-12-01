hammerlab/genspio

commit 9ca004137b805ed054511ab88d9ac6b02a2754ea

Add `?default` arguments to CLI-options

src/lib/language.ml
@@ -34,13 +34,14 @@ module Literal = struct
 
 end
 
-type cli_option = {
+type 'a cli_option = {
   switch: char;
   doc: string;
+  default: 'a;
 }
 type _ option_spec =
-  | Opt_flag: cli_option -> bool t option_spec
-  | Opt_string: cli_option -> string t option_spec
+  | Opt_flag:   bool t cli_option -> bool t option_spec
+  | Opt_string: string t cli_option -> string t option_spec
 and (_, _) cli_options =
   | Opt_end: string -> ('a, 'a) cli_options
   | Opt_cons: 'c option_spec * ('a, 'b) cli_options -> ('c -> 'a, 'b) cli_options
@@ -110,7 +111,7 @@ module Construct = struct
   let literal l = Literal l
   let string s = Literal.String s |> literal
   let int s = Literal.Int s |> literal
-  let bool = Literal.Bool true |> literal
+  let bool t = Literal.Bool t |> literal
 
   let output_as_string e = Output_as_string e
 
@@ -120,8 +121,10 @@ module Construct = struct
   let loop_while condition ~body = While {condition; body}
 
   module Option_list = struct
-    let string ~doc switch  = Opt_string {switch; doc}
-    let flag ~doc switch = Opt_flag {switch; doc}
+    let string ?(default = string "") ~doc switch =
+      Opt_string {switch; doc; default}
+    let flag ?(default = bool false) ~doc switch =
+      Opt_flag {switch; doc; default}
 
     let (&) x y = Opt_cons (x, y)
     let usage s = Opt_end s
@@ -242,7 +245,8 @@ let rec to_shell: type a. _ -> a t -> string =
             f
           | Opt_cons (Opt_string x, more) ->
             let var = variable x in
-            to_init (sprintf "export %s= " var);
+            to_init (sprintf "export %s=$(%s)"
+                       var (continue x.default |> expand_octal));
             to_case (sprintf "-%c) %s ;;"
                        x.switch
                        (seq [
@@ -259,7 +263,9 @@ let rec to_shell: type a. _ -> a t -> string =
             loop (f (string_of_var var)) more
           | Opt_cons (Opt_flag x, more) ->
             let var = variable x in
-            to_init (sprintf "export %s=1 " var);
+            to_init (sprintf
+                       "export %s=$(if %s then printf '0' else printf '1')" var
+                       (continue x.default));
             to_case (
               sprintf "-%c) %s ;;"
                 x.switch (seq [

src/lib/EDSL.ml
@@ -1,5 +1,5 @@
 type 'a t = 'a Language.t
-type cli_option = Language.cli_option
+type 'a cli_option = 'a Language.cli_option
 type 'a option_spec = 'a Language.option_spec
 type ('a, 'b) cli_options = ('a, 'b) Language.cli_options
 include Language.Construct

src/lib/EDSL.mli
@@ -1,7 +1,4 @@
 type 'a t = 'a Language.t
-type cli_option = Language.cli_option
-type 'a option_spec = 'a Language.option_spec
-type ('a, 'b) cli_options = ('a, 'b) Language.cli_options
 
 
 val fail: unit t
@@ -45,18 +42,29 @@ val write_output :
   ?return_value:string -> unit t -> unit t
 val write_stdout : path:string -> unit t -> unit t
 
+(** {3 Literals } *)
+
 val string : string -> string t
 val int : int -> int t
-val bool : bool t
+val bool : bool -> bool t
+
 val output_as_string : unit t -> string t
 val feed : string:string t -> unit t -> unit t
 val ( >> ) : string t -> unit t -> unit t
 val loop_while : bool t -> body:unit t -> unit t
 
+type 'a cli_option = 'a Language.cli_option
+type 'a option_spec = 'a Language.option_spec
+type ('a, 'b) cli_options = ('a, 'b) Language.cli_options
 module Option_list : sig
   val string :
-    doc:string -> char -> string t option_spec
-  val flag : doc:string -> char -> bool t option_spec
+    ?default: string t ->
+    doc:string -> char ->
+    string t option_spec
+  val flag :
+    ?default: bool t ->
+    doc:string -> char ->
+    bool t option_spec
   val ( & ) :
     'a option_spec ->
     ('b, 'c) cli_options -> ('a -> 'b, 'c) cli_options

commit 655d3dc2ebd65e661b98bd5752d03e1584cf3fd9

Fix compilation bug introduced in `9ca004137b`
Bug [caught](https://travis-ci.org/hammerlab/genspio/builds/180239928) by
Travis.

src/lib/language.ml
@@ -264,7 +264,7 @@ let rec to_shell: type a. _ -> a t -> string =
           | Opt_cons (Opt_flag x, more) ->
             let var = variable x in
             to_init (sprintf
-                       "export %s=$(if %s then printf '0' else printf '1')" var
+                       "export %s=$(if %s ; then printf '0' ; else printf '1' ; fi)" var
                        (continue x.default));
             to_case (
               sprintf "-%c) %s ;;"