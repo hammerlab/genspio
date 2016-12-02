a9f8b9be6affe3996ea910d64bce4c51b2bcccf4
Sebastien Mondet <seb@mondet.org>
2016-12-02 17:42:09 -0500

    Improve the “downloader” example
    The generated script now extracts GPG, Gzip, and Tar files (in a loop).
    
    Error management has also been improved.

--- src/test/examples.ml
+++ src/test/examples.ml
@@ -22,31 +22,69 @@ let downloader () =
   let fail l = seq [say (string "ERROR: " :: l); fail] in
   let failf fmt = ksprintf (fun s -> fail [string s]) fmt in
   let (//) = Filename.concat in
+  let module Switch = struct
+    let case condition body = `Case (condition, seq body)
+    let default d = `Default d
+    let switch l =
+      let default = ref None in
+      let cases =
+        List.filter_map l ~f:(function
+          | `Default d when !default <> None ->
+            failwith "Cannot build switch with >1 defaults"
+          | `Default d -> default := (Some (seq d)); None
+          | `Case t -> Some t)
+      in
+      switch ~default:(Option.value ~default:nop !default) cases
+  end in
+  let module If = struct
+    let make ~t ?e c =
+      match e with
+      | None -> if_then c (seq t)
+      | Some f -> if_then_else c (seq t) (seq f) 
+  end in
+  let silent ~name unit =
+    object (self)
+      method stdout = "/tmp" // sprintf "output-of-%s-%s" name "-out"
+      method stderr = "/tmp" // sprintf "output-of-%s-%s" name "-err"
+      method exec =
+        seq [
+          (* sayf "Silent %s (%s, %s)" name self#stdout self#stderr; *)
+          write_output (seq unit) ~stdout:self#stdout ~stderr:self#stderr;
+        ]
+      method succeed_or_fail =
+        If.(make
+              (self#exec|> succeeds)
+              ~t:[sayf "%s: Success" name]
+              ~e:[
+                sayf "Expression %s failed!" name;
+                exec ["cat"; self#stderr];
+                failf "Fatal failure of %s" name;
+              ])
+    end in
   let silence ~name unit =
-    let stdout = "/tmp" // sprintf "output-of-%s-%s" name "-out" in
-    let stderr = "/tmp" // sprintf "output-of-%s-%s" name "-err" in
-    seq [
-      sayf "Silent %s (%s, %s)" name stdout stderr;
-      write_output unit ~stdout ~stderr;
-    ] in
+    let s = silent ~name [unit] in
+    s#exec in
+  let succeed_in_silence_or_fail ~name units =
+    let s = silent ~name units in
+    s#succeed_or_fail in
   let download ~url ~output =
-    switch [
-      exec ["wget"; "--help"] |> silence ~name:"wget-help" |> succeeds,
-      seq [
-        sayf "Using WGET.";
-        call [
-          string "wget"; url; string "--output-document"; output;
-        ];
-      ];
-      exec ["curl"; "--help"] |> silence ~name:"curl-help" |> succeeds,
-      seq [
-        sayf "Using CURL.";
-        call [
-          string "curl"; url; string "--output"; output;
-        ];
-      ];
-    ]
-      ~default:(failf "Can't find a downloading application")
+    let try_help ?(opt = "--help") cmd =
+      exec [cmd; opt] |> silence ~name:(cmd ^ opt) |> succeeds in
+    let do_call exec args = [
+      sayf "Using `%s`." exec;
+      succeed_in_silence_or_fail ~name:exec [
+        call (string exec :: args);
+      ]
+    ] in
+    Switch.(
+      switch [
+        case (try_help "wget")
+          (do_call "wget" [url; string "--output-document"; output]);
+        case (try_help "curl")
+          (do_call "wget" [url; string "--output-document"; output]);
+        default [failf "Can't find a downloading application"];
+      ]
+    )
   in
   let string_matches_any string regexp_list =
     (* Cf. http://pubs.opengroup.org/onlinepubs/009695399/utilities/grep.html *)
@@ -61,17 +99,34 @@ let downloader () =
       string
         ~doc:"The URL to the stuff" 'u'
         ~default:no_value
-      & usage "$0 -u URL"
+      & flag 'c' ~doc:"Do everything in the temp-dir"
+      & usage "$0 -u URL [-c]"
     )
-    begin fun url ->
+    begin fun url all_in_tmp ->
       let filename =
         url >> exec ["sed"; "s:.*/\\([^\\?\\/]*\\)\\?.*:\\1:"]
         |> output_as_string in
       let tmpdir = "/tmp/genspio-downloader" in
       let output_of_download =
         string_concat [string tmpdir; string "/"; filename] in
+      let current_name =
+        let path = tmpdir // "current-name" in
+        let tmp = path ^ "-tmp" in
+        object
+          method get = output_as_string (exec ["cat"; path])
+          method set v =
+            seq [
+              v >> exec ["cat"] |> write_output ~stdout:tmp;
+              exec ["mv"; "-f"; tmp; path];
+            ]
+        end in
+      let remove_suffix v suf =
+        v >> exec ["sed"; sprintf "s:^\\(.*\\)%s$:\\1:" suf]
+        |> output_as_string in
       seq [
         exec ["mkdir"; "-p"; tmpdir];
+        if_then all_in_tmp
+          (seq [sayf "Going to the tmpdir"; call [string "cd"; string tmpdir]]);
         if_then (url =$= no_value)
           (failf "Argument URL is mandatory");
         if_then_else
@@ -79,6 +134,47 @@ let downloader () =
           (seq [
               download ~url ~output:output_of_download;
               say [string "Downloaded "; output_of_download];
+              current_name#set output_of_download;
+              loop_while 
+                (string_matches_any current_name#get [".gpg$"; ".tgz$"; ".tar$"; ".gz$"])
+                ~body:begin
+                  let make_case ~ext ~verb commands =
+                    Switch.case (string_matches_any
+                                   current_name#get [sprintf ".%s$" ext]) [
+                      say [ksprintf string "%s: " verb; current_name#get];
+                      succeed_in_silence_or_fail
+                        ~name:(sprintf "%s-%s" verb ext) commands;
+                      current_name#set
+                        (remove_suffix current_name#get (sprintf "\\.%s" ext));
+                    ] in
+                  seq [
+                    say [string "Extract loop: "; current_name#get];
+                    Switch.(switch [
+                        make_case ~ext:"gz" ~verb:"Gunzipping" [
+                          call [string "gunzip"; current_name#get];
+                        ];
+                        make_case ~ext:"tar" ~verb:"Untarring" [
+                          call [string "tar"; string "xf"; current_name#get];
+                        ];
+                        make_case ~ext:"tgz" ~verb:"Untar-gzip-ing" [
+                          call [string "tar"; string "zxf"; current_name#get];
+                        ];
+                        make_case ~ext:"gpg" ~verb:"Decyphering" [
+                          call [string "gpg";
+                                string "--output";
+                                (remove_suffix current_name#get "\\.gpg");
+                                string "-d"; current_name#get;];
+                        ];
+                        default [
+                          fail [
+                            string "File: "; current_name#get;
+                            string " didn't match any option???"
+                          ];
+                        ];
+                      ];
+                      );
+                  ]
+                end
             ])
           (seq [
               fail [
