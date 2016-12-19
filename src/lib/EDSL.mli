728ea4f044e906681f838c7d481af4fd8d1e3b3f
Sebastien Mondet <seb@mondet.org>
2016-12-19 15:09:32 -0500

    Fix documentation and oredering in `EDSL.mli`
    Cf. comments in PR #11.

--- src/lib/EDSL.mli
+++ src/lib/EDSL.mli
@@ -38,16 +38,18 @@ val if_then_else :
   bool t -> unit t -> unit t -> unit t
 val if_then : bool t -> unit t -> unit t
 
+val seq : unit t list -> unit t
+(** Sequence a list of expressions into an expression. *)
+
 val if_seq:
   t:unit t list ->
   ?e:unit t list ->
   bool t ->
   unit t
-(** [condition c ~t ~e] is an alternate API for {!if_then_else} (when
+(** [if_seq c ~t ~e] is an alternate API for {!if_then_else} (when
     [?e] is provided) or {!if_then} (otherwise) that assumes “then”
     and “else” bodies to be lists for {!seq} construct. *)
 
-val seq : unit t list -> unit t
 val not : bool t -> bool t
 
 val file_exists : string t -> bool t
