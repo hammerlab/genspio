a99b18c617ac14b3f811276dcf44aeed3d872783
Sebastien Mondet <seb@mondet.org>
2017-03-02 15:51:37 -0500

    Set `single_test_timeout` to 20 s for Travis

--- tools/travis_ci_test.sh
+++ tools/travis_ci_test.sh
@@ -70,6 +70,7 @@ gennspio_test=_build/src/test/genspio-test.byte
 genspio_examples=_build/src/test/genspio-examples.byte
 
 echo "================== TEST 0 ======================================================"
+export single_test_timeout=20.
 $gennspio_test
 
 # We also run the example
