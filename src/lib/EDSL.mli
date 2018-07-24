90a638972a35beb58ccb33b3d6e6b9a3281318c6
Sebastien Mondet <seb@mondet.org>
2018-07-24 10:22:28 -0400

    Apply `ocamlformat` #WhiteSpace

--- src/lib/EDSL.mli
+++ src/lib/EDSL.mli
@@ -1,19 +1,18 @@
 (** The Embedded Domain Specific Lanaguage to create “shell-expressions.” *)
 
-
-type 'a t = 'a Language.t
 (** The type of a Genspio expression. *)
+type 'a t = 'a Language.t
 
-type byte_array
 (** Abstract type to encode arbitrary byte-arrays in the EDSL as
     [byte_array t] values, OCaml literal strings or the outputs (as in
     [stdout]) of processes are byte-arrays. *)
+type byte_array
 
-type c_string
 (** Abstract type to encode NUL-terminated strings in the EDSL as
     [c_string t] values. C-strings cannot contain the ['\x00'] character.
     The command line arguments of commands as well as the contents of
     environment variables must be C-strings. *)
+type c_string
 
 (** {3 Literals } *)
 
@@ -32,11 +31,11 @@ val bool : bool -> bool t
 
 (** {3 Comments} *)
 
-val comment: string -> 'a t -> 'a t
+val comment : string -> 'a t -> 'a t
 (** Add a “comment” string to an expression (will be displayed in
     error messages happening inside the expression). *)
 
-val (%%%): string -> 'a t -> 'a t
+val ( %%% ) : string -> 'a t -> 'a t
 (** ["Some comment" %%% expr] is an alias for [comment "Some comment" expr]. *)
 
 (** {3 Basic system Commands} *)
@@ -50,14 +49,14 @@ val exec : string list -> unit t
     actually [call [string "a"; string "b"]] which is the usual shell command
     ["a b"] (with proper escaping). *)
 
-val getenv: c_string t -> c_string t
+val getenv : c_string t -> c_string t
 (** Get the value of an environment variable as a string;
     it returns the empty string when the variable is not defined.
     If the argument is not a valid variable name, behavior is
     undefined.
  *)
 
-val setenv: var:c_string t -> c_string t -> unit t
+val setenv : var:c_string t -> c_string t -> unit t
 (** Set the value of an environment variable as a string;
     it returns the empty string is the variable is not defined.
 
@@ -76,10 +75,12 @@ val setenv: var:c_string t -> c_string t -> unit t
 (** {3 Boolean Expressions} *)
 
 val ( &&& ) : bool t -> bool t -> bool t
+
 val ( ||| ) : bool t -> bool t -> bool t
+
 val not : bool t -> bool t
 
-val returns: 'a t -> value: int -> bool t
+val returns : 'a t -> value:int -> bool t
 (** Check the return value of a command/expression/script. *)
 
 val succeeds : 'a t -> bool t
@@ -90,43 +91,71 @@ val file_exists : c_string t -> bool t
     [call [c_string "test"; c_string "-f"; path] |> succeeds]. *)
 
 (** Conversions of the [bool t] type. *)
-module Bool: sig
+module Bool : sig
   val to_string : bool t -> c_string t
+
   val of_string : c_string t -> bool t
 end
-
 
 (** {3 Integer Arithmetic} *)
 
 (** Functions on [int t] values (arithmetic, comparisons, conversions, etc.). *)
 module Integer : sig
   val to_string : int t -> c_string t
+
   val to_byte_array : int t -> byte_array t
+
   val of_string : c_string t -> int t
+
   val of_byte_array : byte_array t -> int t
-  val bin_op : int t -> [ `Div | `Minus | `Mult | `Plus | `Mod ] -> int t -> int t
+
+  val bin_op :
+    int t -> [`Div | `Minus | `Mult | `Plus | `Mod] -> int t -> int t
+
   val add : int t -> int t -> int t
+
   val ( + ) : int t -> int t -> int t
+
   val sub : int t -> int t -> int t
+
   val ( - ) : int t -> int t -> int t
+
   val mul : int t -> int t -> int t
+
   val ( * ) : int t -> int t -> int t
+
   val div : int t -> int t -> int t
+
   val ( / ) : int t -> int t -> int t
+
   val modulo : int t -> int t -> int t
-  val (mod) : int t -> int t -> int t
-  val cmp : [ `Eq | `Ge | `Gt | `Le | `Lt | `Ne ] -> int t -> int t -> bool t
+
+  val ( mod ) : int t -> int t -> int t
+
+  val cmp : [`Eq | `Ge | `Gt | `Le | `Lt | `Ne] -> int t -> int t -> bool t
+
   val eq : int t -> int t -> bool t
+
   val ne : int t -> int t -> bool t
+
   val lt : int t -> int t -> bool t
+
   val le : int t -> int t -> bool t
+
   val ge : int t -> int t -> bool t
+
   val gt : int t -> int t -> bool t
+
   val ( = ) : int t -> int t -> bool t
+
   val ( <> ) : int t -> int t -> bool t
+
   val ( < ) : int t -> int t -> bool t
+
   val ( <= ) : int t -> int t -> bool t
+
   val ( >= ) : int t -> int t -> bool t
+
   val ( > ) : int t -> int t -> bool t
 end
 
@@ -134,46 +163,48 @@ end
 
 (** Functions on ['a list t] values. *)
 module Elist : sig
-
-  val make: 'a t list -> 'a list t
+  val make : 'a t list -> 'a list t
   (** Make an EDSL list out of an OCaml list. *)
 
-  val append: 'a list t -> 'a list t -> 'a list t
+  val append : 'a list t -> 'a list t -> 'a list t
   (** Concatenate two EDSL lists. *)
 
-  val iter: 'a list t -> f:((unit -> 'a t) -> unit t) -> unit t
+  val iter : 'a list t -> f:((unit -> 'a t) -> unit t) -> unit t
   (** Iterate over a list, the body of the loop [~f] takes as argument
       function that returns the current eletment at the EDSL level. *)
 
-  val to_string: 'a list t -> f:('a t -> byte_array t) -> byte_array t
+  val to_string : 'a list t -> f:('a t -> byte_array t) -> byte_array t
 
-  val of_string: byte_array t -> f:(byte_array t -> 'a t) -> 'a list t
-
+  val of_string : byte_array t -> f:(byte_array t -> 'a t) -> 'a list t
 end
 
 (** {3 String Manipulation} *)
 
-
 module Byte_array : sig
   val ( =$= ) : byte_array t -> byte_array t -> bool t
-  val ( <$> ) : byte_array t -> byte_array t -> bool t
-  val to_c_string: byte_array t -> c_string t
-  val to_c: byte_array t -> c_string t
-end
 
+  val ( <$> ) : byte_array t -> byte_array t -> bool t
+
+  val to_c_string : byte_array t -> c_string t
+
+  val to_c : byte_array t -> c_string t
+end
 
 module C_string : sig
   val equals : c_string t -> c_string t -> bool t
+
   val ( =$= ) : c_string t -> c_string t -> bool t
+
   val ( <$> ) : c_string t -> c_string t -> bool t
 
-  val to_byte_array: c_string t -> byte_array t
-  val to_bytes: c_string t -> byte_array t
+  val to_byte_array : c_string t -> byte_array t
 
-  val concat_list: c_string t list -> c_string t
+  val to_bytes : c_string t -> byte_array t
+
+  val concat_list : c_string t list -> c_string t
   (** Concatenate an (OCaml) list of [c_string t] values. *)
 
-  val concat_elist: c_string list t -> c_string t
+  val concat_elist : c_string list t -> c_string t
   (** Concatenate a Genspio list of strings [c_string list t]. *)
 end
 
@@ -196,52 +227,45 @@ val loop_seq_while : bool t -> unit t list -> unit t
 (** [loop_seq_while condition body] is a shortcut for
     [loop_while condition ~body:(seq body)]. *)
 
-val if_seq:
-  t:unit t list ->
-  ?e:unit t list ->
-  bool t ->
-  unit t
+val if_seq : t:unit t list -> ?e:unit t list -> bool t -> unit t
 (** [if_seq c ~t ~e] is an alternate API for {!if_then_else} (when
     [?e] is provided) or {!if_then} (otherwise) that takes “then”
     and “else” bodies which are lists for the {!seq} construct. *)
 
 (** {3 Switch Statements } *)
 
-val switch :
-  [ `Case of bool t * unit t | `Default of unit t ] list -> unit t
+val switch : [`Case of bool t * unit t | `Default of unit t] list -> unit t
 (** Create a switch statement from a list of {!case} and optionally a
     {!default} (the function raises an exception if there are more
     than one default cases). *)
 
-val case :
-  bool t ->
-  unit t list ->
-  [> `Case of bool t * unit t ]
+val case : bool t -> unit t list -> [> `Case of bool t * unit t]
 (** Create a normal case for a {!switch} statement. *)
 
-val default : unit t list -> [> `Default of unit t ]
+val default : unit t list -> [> `Default of unit t]
 (** Create the default case for a {!switch} statement. *)
 
-
 (**/**)
+
 val make_switch :
-  (bool Language.t * unit Language.t) list ->
-  default:unit Language.t -> unit Language.t
+     (bool Language.t * unit Language.t) list
+  -> default:unit Language.t
+  -> unit Language.t
+
 (**/**)
 
 (** {3 Redirections and File Descriptors } *)
 
-type fd_redirection
 (** Abstract type of file-descriptor redirections. *)
+type fd_redirection
 
-val to_fd: int t -> int t -> fd_redirection
+val to_fd : int t -> int t -> fd_redirection
 (** Create a file-descriptor to  file-descriptor redirection. *)
 
-val to_file: int t -> c_string t -> fd_redirection
+val to_file : int t -> c_string t -> fd_redirection
 (** Create a file-descriptor to file redirection. *)
 
-val with_redirections:
-  unit t -> fd_redirection list -> unit t
+val with_redirections : unit t -> fd_redirection list -> unit t
 (** 
    Run a [unit t] expression after applying a list of file-descriptor
    redirections.
@@ -260,20 +284,22 @@ val with_redirections:
 *)
 
 val write_output :
-  ?stdout:c_string t ->
-  ?stderr:c_string t ->
-  ?return_value:c_string t -> unit t -> unit t
+     ?stdout:c_string t
+  -> ?stderr:c_string t
+  -> ?return_value:c_string t
+  -> unit t
+  -> unit t
 (** Redirect selected streams or the return value to files ([stdout],
     [stderr], [return_value] are paths). *)
 
-val write_stdout : path: c_string t -> unit t -> unit t
+val write_stdout : path:c_string t -> unit t -> unit t
 (** [write_stdout ~path expr] is [write_output expr ~stdout:path]. *)
 
-val pipe: unit t list -> unit t
+val pipe : unit t list -> unit t
 (** Pipe commands together (["stdout"] into ["stdin"] exactly like the
     [" | "] operator). *)
 
-val (||>) : unit t -> unit t -> unit t
+val ( ||> ) : unit t -> unit t -> unit t
 (** [a ||> b] is a shortcut for [pipe [a; b]]. *)
 
 val get_stdout : unit t -> byte_array t
@@ -295,26 +321,24 @@ val eprintf : c_string t -> c_string t list -> unit t
 
 (** {3 Escaping The Execution Flow } *)
 
-val fail: string -> unit t
+val fail : string -> unit t
 (** Expression that aborts the whole script/command immediately, it
     will try to output its argument to [stderr] (but this may be
     silent depending on the redirections active at a given time). *)
 
 (** {3 Temporary Files} *)
 
-type file = <
-  get : byte_array t; (** Get the current contents of the file *)
-  get_c : c_string t;
-  set : byte_array t -> unit t;
-  set_c : c_string t -> unit t;
-  append : byte_array t -> unit t;
-  delete: unit t;
-  path: c_string t;
->
 (** Abstraction of a file, cf. {!tmp_file}. *)
+type file =
+  < get: byte_array t  (** Get the current contents of the file *)
+  ; get_c: c_string t
+  ; set: byte_array t -> unit t
+  ; set_c: c_string t -> unit t
+  ; append: byte_array t -> unit t
+  ; delete: unit t
+  ; path: c_string t >
 
-
-val tmp_file: ?tmp_dir: c_string t -> string -> file
+val tmp_file : ?tmp_dir:c_string t -> string -> file
 (** Create a temporary file that may contain arbitrary strings (can be
     used as variable containing [string t] values).
     
@@ -324,11 +348,10 @@ val tmp_file: ?tmp_dir: c_string t -> string -> file
     it is the same file.
  *)
 
-
 (** {3 Command Line Parsing} *)
 
 (** Typed command-line parsing for your shell scripts, à la {!Printf.scanf}. *)
-module Command_line: sig
+module Command_line : sig
   (** 
 
      Use this module like OCaml's {!Printf.scanf} function.
@@ -384,43 +407,46 @@ module Command_line: sig
      ]}
   *)
 
-  type 'a cli_option = {
-    switches : string list;
-    doc : string;
-    default : 'a;
-  }
+  type 'a cli_option = {switches: string list; doc: string; default: 'a}
 
   type _ option_spec =
-      Opt_flag : bool t cli_option -> bool t option_spec
-    | Opt_string : c_string t cli_option -> c_string t option_spec
+    | Opt_flag: bool t cli_option -> bool t option_spec
+    | Opt_string: c_string t cli_option -> c_string t option_spec
+
   and (_, _) cli_options =
-      Opt_end : string -> ('a, 'a) cli_options
-    | Opt_cons : 'c option_spec *
-        ('a, 'b) cli_options -> ('c -> 'a, 'b) cli_options
+    | Opt_end: string -> ('a, 'a) cli_options
+    | Opt_cons:
+        'c option_spec * ('a, 'b) cli_options
+        -> ('c -> 'a, 'b) cli_options
 
-  module Arg :
-    sig
-      val string :
-        ?default:c_string t -> doc:string -> string list -> c_string t option_spec
-      val flag :
-        ?default:bool t -> doc:string -> string list -> bool t option_spec
-      val ( & ) :
-        'a option_spec -> ('b, 'c) cli_options -> ('a -> 'b, 'c) cli_options
-      val usage : string -> ('a, 'a) cli_options
-    end
+  module Arg : sig
+    val string :
+         ?default:c_string t
+      -> doc:string
+      -> string list
+      -> c_string t option_spec
 
-  val parse : ('a, unit t) cli_options -> (anon: c_string list t -> 'a) -> unit t
+    val flag :
+      ?default:bool t -> doc:string -> string list -> bool t option_spec
+
+    val ( & ) :
+      'a option_spec -> ('b, 'c) cli_options -> ('a -> 'b, 'c) cli_options
+
+    val usage : string -> ('a, 'a) cli_options
+  end
+
+  val parse :
+    ('a, unit t) cli_options -> (anon:c_string list t -> 'a) -> unit t
 end
 
 (** {3 Additional Higher-Level Utilities} *)
 
-
-
 val loop_until_true :
-  ?attempts:int ->
-  ?sleep:int ->
-  ?on_failed_attempt:(int t -> unit t) ->
-  bool t -> bool t
+     ?attempts:int
+  -> ?sleep:int
+  -> ?on_failed_attempt:(int t -> unit t)
+  -> bool t
+  -> bool t
 (** [loop_until_true eval_condition] tries to run [eval_condition]
     in a loop until it succeeds. It makes [~attempts] attemps
     (default 20), and sleeps for [sleep] seconds (default 2) after
@@ -437,30 +463,30 @@ val silently : unit t -> unit t
 val succeeds_silently : unit t -> bool t
 (**  [succeeds_silently u] {i is} [silently u |> succeeds]. *)
 
-val seq_and: 'a t list -> bool t
+val seq_and : 'a t list -> bool t
 (** [seq_and [a; b; c]] is like [succeeds a &&& succeeds b &&& succeeds c]. *)
 
-val output_markdown_code: string -> unit t -> unit t
+val output_markdown_code : string -> unit t -> unit t
 (** [output_markdown_code "ocaml" (exec ["echo"; "let x = 42"])]
     runs its second argument within markdown-like code fences. *)
 
-val cat_markdown: string -> c_string t -> unit t
+val cat_markdown : string -> c_string t -> unit t
 (** [cat_markdown tag path] outputs the contents of the file at
     [path] (with ["cat"]) within a markdown code bloc. *)
 
-val check_sequence:
-  ?verbosity:[ `Announce of string | `Output_all | `Silent ] ->
-  ?on_failure:(step: string * unit t ->
-               stdout: c_string t ->
-               stderr: c_string t ->
-               unit t) ->
-  ?on_success:(step: string * unit t ->
-               stdout: c_string t ->
-               stderr: c_string t ->
-               unit t) ->
-  ?tmpdir:string ->
-  (string * unit t) list ->
-  unit t
+val check_sequence :
+     ?verbosity:[`Announce of string | `Output_all | `Silent]
+  -> ?on_failure:(   step:string * unit t
+                  -> stdout:c_string t
+                  -> stderr:c_string t
+                  -> unit t)
+  -> ?on_success:(   step:string * unit t
+                  -> stdout:c_string t
+                  -> stderr:c_string t
+                  -> unit t)
+  -> ?tmpdir:string
+  -> (string * unit t) list
+  -> unit t
 (** Run a sequence of expressions until the first that fails:
 
     {ul
@@ -489,19 +515,18 @@ val check_sequence:
 
 *)
 
-val on_stdin_lines: (c_string t -> unit t) -> unit t
+val on_stdin_lines : (c_string t -> unit t) -> unit t
 (** [on_stdin_lines body] builds a loop that iterates over the lines of the [stdin]
     file descriptor. The argument of `body` is the current line.
     Note that this is for text-like input, ['\000']
     characters in the input lead to undefined behavior. *)
-
 
 (** {3 Very Unsafe Operations} *)
 
 (** The {!Magic} module is like OCaml's {!Obj.magic} function for the
     EDSL; it allows one to bypass typing. *)
 module Magic : sig
-  val unit: string -> unit t
+  val unit : string -> unit t
   (** Put any string as a [unit t] command inline ([Magic.unit s]
       is different from [exec ["sh"; "-c"; s]] there is no escaping or
       protection). *)
--- src/lib/compile.ml
+++ src/lib/compile.ml
@@ -2,138 +2,130 @@ open Common
 
 type 'a t = 'a Language.t
 
-
 let default_max_argument_length = Some 100_000
 
 module To_posix = struct
-
   type internal_error_details = Language.internal_error_details =
     {variable: string; content: string; code: string}
+
   type death_message = Language.death_message =
     | User of string
     | C_string_failure of internal_error_details
     | String_to_int_failure of internal_error_details
-  type death_function =
-    comment_stack: string list -> death_message -> string
 
-  type compilation_error = Language.compilation_error = {
-    error: [
-      | `No_fail_configured of death_message (* Argument of fail *)
-      | `Max_argument_length of string (* Incriminated argument *)
-      | `Not_a_c_string of string (* The actual string *)
-    ];
-    code: string option;
-    comment_backtrace: string list;
-  }
+  type death_function = comment_stack:string list -> death_message -> string
+
+  type compilation_error = Language.compilation_error =
+    { error:
+        [ `No_fail_configured of death_message (* Argument of fail *)
+        | `Max_argument_length of string (* Incriminated argument *)
+        | `Not_a_c_string of string (* The actual string *) ]
+    ; code: string option
+    ; comment_backtrace: string list }
+
   let pp_error = Language.pp_error
+
   let error_to_string = Format.asprintf "%a" pp_error
 
-  type parameters = {
-    style: [ `One_liner | `Multi_line ];
-    max_argument_length: int option;
-    fail_with: [
-      | `Nothing
-      | `Trap_and_kill of int * string
-      | `Kill of string
-    ];
-    print_failure: death_function;
-  }
+  type parameters =
+    { style: [`One_liner | `Multi_line]
+    ; max_argument_length: int option
+    ; fail_with: [`Nothing | `Trap_and_kill of int * string | `Kill of string]
+    ; print_failure: death_function }
 
-  let failure_to_stderr : death_function = fun ~comment_stack msg ->
+  let failure_to_stderr : death_function =
+   fun ~comment_stack msg ->
     let summary s =
-      match String.sub s 0 65 with
-      | Some s -> s ^ " …"
-      | None -> s in
+      match String.sub s 0 65 with Some s -> s ^ " …" | None -> s
+    in
     let open Format in
     let big_string fmt s = Format.fprintf fmt "@[%s@]" (summary s) in
     let msg_str =
       Format.asprintf "@[Error:@ @[%a@]%a@]"
-        (Language.pp_death_message ~style:`User ~big_string) msg
+        (Language.pp_death_message ~style:`User ~big_string)
+        msg
         (fun fmt () ->
-           (match comment_stack with
-           | [] -> fprintf fmt ""
-           | more ->
-             fprintf fmt ";@ Comment-stack:@ @[[%a]@]"
-               (pp_print_list
-                  ~pp_sep:(fun fmt () -> fprintf fmt ",@ ")
-                  (fun fmt s -> fprintf fmt "@[`%s`@]" s))
-               more
-               )) ()
+          match comment_stack with
+          | [] -> fprintf fmt ""
+          | more ->
+              fprintf fmt ";@ Comment-stack:@ @[[%a]@]"
+                (pp_print_list
+                   ~pp_sep:(fun fmt () -> fprintf fmt ",@ ")
+                   (fun fmt s -> fprintf fmt "@[`%s`@]" s))
+                more )
+        ()
       |> Filename.quote
     in
     asprintf " printf -- '%%s\\n' %s >&2 " msg_str
 
-  let one_liner = {
-    style = `One_liner;
-    max_argument_length = Some 100_000;
-    fail_with = `Trap_and_kill (78, "USR2");
-    print_failure = failure_to_stderr;
-  }
-  let multi_line = {one_liner with style = `Multi_line }
+  let one_liner =
+    { style= `One_liner
+    ; max_argument_length= Some 100_000
+    ; fail_with= `Trap_and_kill (78, "USR2")
+    ; print_failure= failure_to_stderr }
+
+  let multi_line = {one_liner with style= `Multi_line}
+
   let default_options = one_liner
 
-  let string_exn ?(options = default_options) term =
+  let string_exn ?(options= default_options) term =
     let statement_separator =
-      match options.style with
-      | `Multi_line -> "\n"
-      | `One_liner -> " ; " in
+      match options.style with `Multi_line -> "\n" | `One_liner -> " ; "
+    in
     let {max_argument_length; print_failure} = options in
     let open Language in
     match options.fail_with with
     | `Nothing ->
-      to_shell {statement_separator; die_command = None; max_argument_length} term
+        to_shell
+          {statement_separator; die_command= None; max_argument_length}
+          term
     | `Kill signal_name ->
-      with_die_function ~print_failure ~statement_separator ~signal_name (fun ~die ->
-          to_shell {statement_separator;
-                    die_command = Some die;
-                    max_argument_length} term)
+        with_die_function ~print_failure ~statement_separator ~signal_name
+          (fun ~die ->
+            to_shell
+              {statement_separator; die_command= Some die; max_argument_length}
+              term )
     | `Trap_and_kill (ret, signal) ->
-      with_die_function ~print_failure ~statement_separator ~signal_name:signal
-        ~trap:(`Exit_with ret)
-        (fun ~die ->
-           to_shell {statement_separator;
-                     die_command = Some die;
-                     max_argument_length} term)
+        with_die_function ~print_failure ~statement_separator
+          ~signal_name:signal ~trap:(`Exit_with ret) (fun ~die ->
+            to_shell
+              {statement_separator; die_command= Some die; max_argument_length}
+              term )
 
   let string ?options term =
     match string_exn ?options term with
     | s -> Ok s
-    | exception (Language.Compilation ce) -> Error ce
-
+    | exception Language.Compilation ce -> Error ce
 end
 
-let to_legacy style
-    ?(max_argument_length = default_max_argument_length) ?(no_trap = false) e =
-  To_posix.string e ~options:{
-    style; max_argument_length;
-    fail_with = begin
-      if no_trap then `Nothing else `Trap_and_kill (77, "USR1")
-    end;
-    print_failure = To_posix.failure_to_stderr;
-  }
-  |> function
-  | Ok s -> s
-  | Error e ->
-    failwith @@ To_posix.error_to_string e
+let to_legacy style ?(max_argument_length= default_max_argument_length)
+    ?(no_trap= false) e =
+  To_posix.string e
+    ~options:
+      { style
+      ; max_argument_length
+      ; fail_with= (if no_trap then `Nothing else `Trap_and_kill (77, "USR1"))
+      ; print_failure= To_posix.failure_to_stderr }
+  |> function Ok s -> s | Error e -> failwith @@ To_posix.error_to_string e
 
 let to_one_liner ?max_argument_length ?no_trap e =
-  to_legacy `One_liner
-    ?max_argument_length ?no_trap e
+  to_legacy `One_liner ?max_argument_length ?no_trap e
+
 let to_many_lines ?max_argument_length ?no_trap e =
-  to_legacy `Multi_line
-    ?max_argument_length ?no_trap e
+  to_legacy `Multi_line ?max_argument_length ?no_trap e
 
 let quick_run_exn ?max_argument_length ?no_trap e =
   match to_many_lines ?max_argument_length ?no_trap e |> Sys.command with
   | 0 -> ()
   | other -> ksprintf failwith "Command returned %d" other
 
-
 let pp_hum = Language.pp
+
 let to_string_hum e = Format.asprintf "%a" pp_hum e
+
 let to_one_line_hum e =
   let buf = Buffer.create 42 in
-  let formatter = Format.formatter_of_buffer  buf in
-  Format.pp_set_margin formatter 10_000_000;
-  Format.fprintf formatter "@[<h>%a@]@?" pp_hum e;
+  let formatter = Format.formatter_of_buffer buf in
+  Format.pp_set_margin formatter 10_000_000 ;
+  Format.fprintf formatter "@[<h>%a@]@?" pp_hum e ;
   Buffer.contents buf
