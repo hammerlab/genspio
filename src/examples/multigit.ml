28a8b551a0d9e79de95faaf7a1776d60d03c0d45
Sebastien Mondet <seb@mondet.org>
2018-10-25 14:33:34 -0400

    Add option `--show-modified` (`git multi-status`)

--- src/examples/multigit.ml
+++ src/examples/multigit.ml
@@ -36,8 +36,11 @@ let multi_status () =
   end) in
   let opts =
     let open Arg in
-    flag ["--version"] ~doc:"Display option" & D.describe_option_and_usage ()
+    flag ["--show-modified"] ~doc:"Show modified files"
+    & flag ["--version"] ~doc:"Show version number"
+    & D.describe_option_and_usage ()
   in
+  let out f l = printf (ksprintf str "%s\n" f) l in
   let list_repos paths =
     call
       ( [str "find"] @ paths
@@ -55,29 +58,36 @@ let multi_status () =
   let behind_branches = branches_vv ||> grep "behind" in
   let get_count u = get_stdout_one_line (u ||> exec ["wc"; "-l"]) in
   let repo_name p = call [str "basename"; p] |> get_stdout_one_line in
-  parse opts (fun ~anon version describe ->
+  let display_all ~show_modified list_of_paths =
+    Elist.iter list_of_paths ~f:(fun p ->
+        seq
+          [ out (sprintf "%s\n>> %%-28s" (String.make 80 '-')) [p ()]
+          ; list_repos [p ()]
+            ||> on_stdin_lines (fun line ->
+                    seq
+                      [ call [str "cd"; line]
+                      ; out
+                          "%-30s | U: %-6s | M: %-4s | Ahead: %-4s | Behind: \
+                           %-4s"
+                          ( repo_name line
+                          :: List.map ~f:get_count
+                               [ untracked_files
+                               ; modified_files
+                               ; ahead_branches
+                               ; behind_branches ] )
+                      ; if_seq
+                          ( show_modified
+                          &&& Str.(get_count modified_files <$> str "0") )
+                          ~t:
+                            [ out "  |- Modified:" []
+                            ; modified_files ||> exec ["sed"; "s:^ M:  |    -:"]
+                            ] ] ) ] )
+  in
+  parse opts (fun ~anon show_modified version describe ->
       D.deal_with_describe describe
         [ if_seq version
-            ~t:[say (sprintf "%s: %s" D.name version_string) []]
-            ~e:
-              [ Elist.iter anon ~f:(fun p ->
-                    seq
-                      [ say
-                          (sprintf "%s\n>> %%-28s" (String.make 80 '-'))
-                          [p ()]
-                      ; list_repos [p ()]
-                        ||> on_stdin_lines (fun line ->
-                                seq
-                                  [ call [str "cd"; line]
-                                  ; say
-                                      "%-30s | U: %-6s | M: %-4s | AB: %-4s | \
-                                       BB: %-4s"
-                                      ( repo_name line
-                                      :: List.map ~f:get_count
-                                           [ untracked_files
-                                           ; modified_files
-                                           ; ahead_branches
-                                           ; behind_branches ] ) ] ) ] ) ] ] )
+            ~t:[out (sprintf "%s: %s" D.name version_string) []]
+            ~e:[display_all ~show_modified anon] ] )
 
 let () =
   let path = Sys.argv.(1) in
--- tools/multigit-test.sh
+++ tools/multigit-test.sh
@@ -35,3 +35,6 @@ try_cmd "git multi-status $moregits 2>&1 | grep ketrew   | egrep 'M: *0'"
 try_cmd "git multi-status $moregits 2>&1 | grep biokepi  | egrep 'M: *2'"
 try_cmd "git multi-status $moregits 2>&1 | grep coclobas | egrep 'U: *1'"
 
+try_cmd "git multi-status $moregits 2>&1 --show-modified  | grep 'README.md'"
+try_cmd "git multi-status $moregits 2>&1 --show-modified  | grep 'LICENSE'"
+
